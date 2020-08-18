3e3edae86d2d1ca47e0d45df98734f9e655f0ae7
subaru-git <subaru12081985@yahoo.co.jp>
2020-08-18 23:01:27 +0900

    add toggle video, show device setting, show chat

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,15 +1,36 @@
-chrome.runtime.onMessage.addListener(msg => {
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+chrome.runtime.onMessage.addListener(async msg => {
   const {
     command
   } = msg;
-  console.log(command);
   switch (command) {
-    case 'toggle-mute':
+    case 'toggle-mute': {
       const mute = document.getElementById('microphone-button');
       if (mute) mute.click();
-      break;
+    }
+    break;
+  case 'toggle-video': {
+    const video = document.getElementById('video-button');
+    if (video) video.click();
+  }
+  break;
+  case 'show-device-setting': {
+    const other = document.getElementById('callingButtons-showMoreBtn')
+    if (other) other.click();
+    console.log(other);
+    await _sleep(100);
+    const device = document.getElementById('device-settings-button');
+    console.log(device);
+    if (device) device.click();
+  }
+  break;
+  case 'show-chat': {
+    const chat = document.getElementById('chat-button');
+    if (chat) chat.click();
+  }
+  break;
 
-    default:
-      break;
+  default:
+    break;
   }
 })
